c895d85c4df554cf78f64d36878cde0f0e5ca8d4
Anish Hallan <anishhallan@gmai.com>
2016-02-23 00:48:09 +0000

    finished_checkpoint

---
+++ app/main_view_controller.rb
@@ -0,0 +1,21 @@
+ class MainViewController < UIViewController
+
+  def loadView
+    self.title = 'Tasks'
+    self.view = UIView.new 
+    view.backgroundColor = UIColor.whiteColor 
+
+    @button = UIButton.new
+    @button.setTitle("Add task", forState: UIControlStateNormal)
+    @button.setTitleColor(UIColor.blueColor, forState:UIControlStateNormal)
+    @button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
+    @button.frame = [[20,250],[280,30]] 
+    view.addSubview(@button)
+  end
+
+  def add_task
+    navigationController.pushViewController(MainViewController.new, animated:true) 
+  end
+
+
+end
