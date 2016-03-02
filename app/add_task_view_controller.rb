785771ba8fd8b9e85213824ead52fe064e2d67b4
Anish Hallan <anishhallan@gmai.com>
2016-03-02 12:06:29 +0000

    Fixed issue with inheritance

--- app/add_task_view_controller.rb
+++ app/add_task_view_controller.rb
@@ -1,24 +1,19 @@
-class AddTaskViewController <UIViewController
+class AddTaskViewController < UIViewController
   attr_accessor :task
-  
+
   def loadView
+    self.title = "Add Task"
     self.view = UIView.new
     view.backgroundColor = UIColor.whiteColor
 
     @text_field = UITextField.new
-    @text_field.frame = [[20,100],[280,50]]
+    @text_field.frame = [[20, 100], [280, 50]]
     @text_field.backgroundColor = UIColor.lightGrayColor
-    @text_field.placeholder = 'Enter your task'
+    @text_field.placeholder = "Enter your task"
     view.addSubview(@text_field)
   end
 
-  def viewWillDissapear(animated)
+  def viewWillDisappear(animated)
     task.title = @text_field.text
   end
-
-  def viewDidLoad
-    self.title = 'Add task'
-  end
-
 end
-
--- app/main_view_controller.rb
+++ app/main_view_controller.rb
@@ -1,32 +1,41 @@
- class MainViewController < UIViewController
+class MainViewController < UIViewController
+  attr_accessor :task
+
+  def init
+    super
+
+    self.task = Task.new
+
+    self
+  end
+
   def loadView
-    self.view = UIView.new 
+    self.title = "Tasks"
+    self.view = UIView.new
     view.backgroundColor = UIColor.whiteColor
 
     @button = UIButton.new
-    @button.setTitle("Add task", forState: UIControlStateNormal)
-    @button.setTitleColor(UIColor.blueColor, forState:UIControlStateNormal)
-    @button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
-    @button.frame = [[20,250],[280,30]] 
+    @button.setTitle("Set Task", forState: UIControlStateNormal)
+    @button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
+    @button.frame = [[20, 100], [280, 30]]
     view.addSubview(@button)
 
     @label = UILabel.new
-    @label.frame = [[20,150],[280,30]]
+    @label.frame = [[20, 150], [280, 30]]
     view.addSubview(@label)
   end
 
   def viewDidLoad
-    self.title = 'Tasks'
-    @task = Task.new #add this here, as only want this to happen once otherwise it'll be replaced
+    @button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
   end
 
   def viewWillAppear(animated)
-    @label.text = @task.title
+    @label.text = task.title
   end
 
   def add_task
     vc = AddTaskViewController.new
-    vc.task = @task
-    navigationController.pushViewController(vc, animated:true) 
+    vc.task = task
+    navigationController.pushViewController(vc, animated: true)
   end
 end
