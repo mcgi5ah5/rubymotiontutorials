255fec8ec740b2b63cbf27c513dbb88aefceed30
Anish Hallan <anishhallan@gmai.com>
2016-03-04 15:47:07 +0000

    finished tutorial

--- app/main_view_controller.rb
+++ app/main_view_controller.rb
@@ -11,29 +11,26 @@ class MainViewController < UIViewController
 
   def loadView
     self.title = "Tasks"
-    self.view = UIView.new
-    view.backgroundColor = UIColor.whiteColor
-
-    @button = UIButton.new
-    @button.setTitle("Set Task", forState: UIControlStateNormal)
-    @button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
-    @button.frame = [[20, 100], [280, 30]]
-    view.addSubview(@button)
-
-    @label = UILabel.new
-    @label.frame = [[20, 150], [280, 30]]
-    view.addSubview(@label)
+    self.view = TasksView.new  
   end
 
   def viewDidLoad
-    @button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
+    view.button.addTarget(self, action: :add_task, forControlEvents: UIControlEventTouchUpInside)
   end
 
   def viewWillAppear(animated)
-    @label.text = task.title
+    view.label.text = task.title
   end
 
   def add_task
+    UIAlertView.alloc.initWithTitle(
+      "Hello world",
+      message: "I hope you have a nice day",
+      delegate: nil,
+      cancelButtonTitle: "NO!" ,
+      otherButtonTitles: "Thank you", nil
+    ).show
+
     vc = AddTaskViewController.new
     vc.task = task
     navigationController.pushViewController(vc, animated: true)
